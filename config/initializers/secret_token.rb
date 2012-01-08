46584951ec0805121f58ce091fedb2a6bdd43f10
damtur <damtur@interia.pl>
2012-01-08 17:59:48 +0000

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coursework::Application.config.secret_token = 'f6084982161ef76cc85cb4ecadd1e16d1ad685b9b4aca98cf3d4dae75e504c23749036e8475fae5d404e3a6743ae2b6c66cfb37683ebbfad3c9e80e8c982f911'
